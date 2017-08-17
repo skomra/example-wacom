a6d503904cc41d5e455e7d5a5043473de62498db
Aaron Armstrong Skomra <skomra@gmail.com>
2017-08-17 15:57:13 -0700

    add pressure.c

---
+++ kernel/pressure.c
@@ -0,0 +1,51 @@
+/*
+ * to compile:
+ *  gcc -o pressure pressure.c  
+ *
+ * to run:
+ *  find your device in /dev/input/...
+ *  sudo ./event-log /dev/input/eventX
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/input.h>
+
+int main (int argc, const char * argv[]) {
+
+	int fd = -1;
+	size_t sz;
+	struct input_event events[128];
+	int i;
+
+	if ((fd = open(argv[1], O_RDONLY)) < 0) {
+		perror("fd open error!");
+		exit(1);
+	}
+
+	while (1) {
+		sz = read(fd, events, sizeof(struct input_event) * 128);
+
+		if (sz < (int) sizeof(struct input_event)) {
+			perror("size error!");
+			exit (1);
+		}
+
+		for (i = 0; i < (int) (sz / sizeof(struct input_event)); i++) {
+
+			if (events[i].type != 3)
+				continue;
+
+			if (events[i].code == ABS_X)
+                        	printf("x value %d\n", events[i].value);
+			if (events[i].code == ABS_Y)
+                        	printf("			y value %d\n", events[i].value);
+			if (events[i].code == ABS_PRESSURE)
+                        	printf("						pressure value %d\n", events[i].value);
+		}
+	}
+
+	return 0;
+}
